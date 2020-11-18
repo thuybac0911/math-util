d1d8ab36d74a437d4c3f6d55a365bbe40b71676d
thuybac0911 <lethuybacvt@gmail.com>
2020-11-18 09:58:20 +0700

    test GREEN, RED

--- test/bacltt/util/test/MathUtilityTest.java
+++ test/bacltt/util/test/MathUtilityTest.java
@@ -42,7 +42,7 @@ public class MathUtilityTest {
         assertEquals(24, getFactorial(4));
         assertEquals(720, getFactorial(6));
         assertEquals(1, getFactorial(0));
-//        assertEquals(0, getFactorial(0));
+        assertEquals(0, getFactorial(0));
         //hay tinh gium toi 5! coi co no co la 120 khong?
         // co thi bao mau xanh, ko thi bao do
         
